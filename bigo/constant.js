8d36e496b88e6e1c0247a6d03626f205e0cd0d71
Justin Green <justin.green@hobbylobby.com>
2025-05-22 14:30:54 -0500

    initial big o

---
+++ bigo/constant.js
@@ -0,0 +1,4 @@
+// O (1)
+ // Constant time complexity, the algorithm will always execute in the same time regardless of the size of the input data set.
+const arr = [1,2,3];
+console.log(arr[arr.length - 1]); // 3
---
+++ bigo/linear.js
@@ -0,0 +1,9 @@
+// O(n)
+// Linear time complexity, running time increases linearly with the size of the input data
+// think of counting from 1 to 100;
+
+const arr = [1,2,3,4,5,6,7];
+
+for(let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
