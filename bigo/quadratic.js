c1c90531a39ead233adba68b7f2c95064751ffc9
Justin Green <justin.green@hobbylobby.com>
2025-05-22 16:58:07 -0500

    quadratic

---
+++ bigo/quadratic.js
@@ -0,0 +1,12 @@
+// O(n^2)
+// Quadratic also known as Polynomial
+const arr = [1,2,3,4,5,6,7];
+
+for(let i = 0; i < arr.length; i++) {
+  console.log(i);
+  for(let j = 1; j < arr.length; j++) {
+    console.log(arr[j]);
+  }
+}
+
+// a nested for loop is an easy way to remember this time complexity
